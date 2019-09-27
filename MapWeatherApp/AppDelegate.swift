dd3d7064a13b3f2d52b44f161cf24f34cfd5f8a2
Isaías <isaias1706@gmail.com>
2019-09-27 12:10:20 -0300

    First commit

---
+++ MapWeatherApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  MapWeatherApp
+//
+//  Created by Isaías López on 9/27/19.
+//  Copyright © 2019 Isaías López. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+
+}
+
---
+++ MapWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapWeatherApp
+//
+//  Created by Isaías López on 9/27/19.
+//  Copyright © 2019 Isaías López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
